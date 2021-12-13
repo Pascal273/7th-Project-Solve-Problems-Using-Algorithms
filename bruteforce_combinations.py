881aca49c5261cb04ad4f4aff59ab97b7ce13d01
Pascal <77117642+Pascal273@users.noreply.github.com>
2021-12-13 14:35:14 +0100

    formated

--- bruteforce_combinations.py
+++ bruteforce_combinations.py
@@ -27,38 +27,38 @@ def create_top_profit_list(csv_dataset):
                 if price > 0 and profit_euro > 0:
                     share_list.append({"name": name, "price(€)": price, "profit(€)": profit_euro})
 
-        # try all combinations and save the ones with total costs below MAX_TO_SPEND
-        possible_combinations = []
-        for r in range(len(share_list) + 1):
-            combinations_list = list(combinations(share_list, r))
-            for combination in combinations_list:
-                total_cost = 0
-                total_profit = 0
-                for share in combination:
-                    total_cost += share["price(€)"]
-                    total_profit += share["profit(€)"]
-                if total_cost < MAX_TO_SPEND:
-                    list_combination = list(combination)
-                    # append "footer" with total of cost and profit
-                    list_combination.append({
-                        "name": "Total",
-                        "price(€)": total_cost,
-                        "profit(€)": total_profit
-                    })
-                    possible_combinations.append({
-                        "profit": total_profit,
-                        "combination": list_combination
-                    })
+    # try all combinations and save the ones with total costs below MAX_TO_SPEND
+    possible_combinations = []
+    for r in range(len(share_list) + 1):
+        combinations_list = list(combinations(share_list, r))
+        for combination in combinations_list:
+            total_cost = 0
+            total_profit = 0
+            for share in combination:
+                total_cost += share["price(€)"]
+                total_profit += share["profit(€)"]
+            if total_cost < MAX_TO_SPEND:
+                list_combination = list(combination)
+                # append "footer" with total of cost and profit
+                list_combination.append({
+                    "name": "Total",
+                    "price(€)": total_cost,
+                    "profit(€)": total_profit
+                })
+                possible_combinations.append({
+                    "profit": total_profit,
+                    "combination": list_combination
+                })
 
-        # get the combination with the highest profit in €
-        best_result = max(possible_combinations, key=lambda x: x["profit"])
-        best_combination = best_result["combination"]
+    # get the combination with the highest profit in €
+    best_result = max(possible_combinations, key=lambda x: x["profit"])
+    best_combination = best_result["combination"]
 
-        # Create and save a new CSV-file from the most profit list
-        Path("Bruteforce CSV Files").mkdir(parents=True, exist_ok=True)
-        file_name = "top shares of " + csv_dataset.split(".")[0]
-        field_names = [key for key, value in best_combination[0].items()]
-        with open(f"Bruteforce CSV Files/{file_name}.csv", "w", newline="", encoding="utf-8") as file:
-            writer = csv.DictWriter(file, fieldnames=field_names)
-            writer.writeheader()
-            writer.writerows(best_combination)
+    # Create and save a new CSV-file from the most profit list
+    Path("Bruteforce CSV Files").mkdir(parents=True, exist_ok=True)
+    file_name = "top shares of " + csv_dataset.split(".")[0]
+    field_names = [key for key, value in best_combination[0].items()]
+    with open(f"Bruteforce CSV Files/{file_name}.csv", "w", newline="", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=field_names)
+        writer.writeheader()
+        writer.writerows(best_combination)
